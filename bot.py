bcf6da1abc4cfe25c9ee5c041a59ad4b558362e6
SawcyD <eadeleke2004@gmail.com>
2024-02-28 01:37:07 -0500

    somthing

--- bot.py
+++ bot.py
@@ -1,9 +1,5 @@
 """
-Copyright © Krypton 2019-2023 - https://github.com/kkrypt0nn (https://krypton.ninja)
-Description:
-🐍 A simple template to start to code your own and personalized discord bot in Python programming language.
-
-Version: 6.1.0
+Version: 0.0.1
 """
 
 import json
